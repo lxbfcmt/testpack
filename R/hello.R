193207d2ae84ae055c70d70f7256314e306fa000
Teng <lxbfc@126.com>
2017-01-11 14:55:34 +1300

    some changes

--- R/hello.R
+++ R/hello.R
@@ -1,20 +1,34 @@
-# Hello, world!
-#
-# This is an example function named 'hello'
-# which prints 'Hello, world!'.
-#
-# You can learn more about package authoring with RStudio at:
-#
-#   http://r-pkgs.had.co.nz/
-#
-# Some useful keyboard shortcuts for package authoring:
-#
-#   Build and Reload Package:  'Ctrl + Shift + B'
-#   Check Package:             'Ctrl + Shift + E'
-#   Test Package:              'Ctrl + Shift + T'
-
-hello <- function() {
-  print("Hello, world!")
+#' A function for doing something
+#'
+#' This function takes some action. It also attempts to create a file on your
+#' desktop called \code{data.txt}. If \code{data.txt} cannot be created a
+#' warning is raised.
+#'
+#' @param force If set to \code{TRUE}, \code{data.txt} will be created on the
+#' user's Desktop if their Desktop exists. If this function is used in an
+#' interactive session the user will be asked whether or not \code{data.txt}
+#' should be created. The default value is \code{FALSE}.
+#'
+#' @export
+some_function <- function(force = FALSE){
+  #
+  # ... some code that does something useful ...
+  #
+  if(!dir.exists(file.path("~", "Desktop"))){
+    warning("No Desktop found.")}
+  else {
+      if(!force && interactive()){
+        result <- select.list(c("Yes", "No"),
+                              title = "May this program create data.txt on your desktop?")
+        if(result == "Yes"){
+          file.create(file.path("~", "Desktop", "data.txt"))
+        }
+      }
+    else if(force){
+        file.create(file.path("~", "Desktop", "data.txt"))
+      }
+    else {
+        warning("data.txt was not created on the Desktop.")
+      }
+    }
 }
-
-sampledata_1= as.matrix(sample(1:500,25),5,5)
